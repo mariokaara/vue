e3ba390570bfbaa6df72e07643b49d56e581c019
Administrator <mario.kaara@gmail.com>
2021-11-01 16:53:58 +0200

    f

---
+++ src/views/Products.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="products">
+    <h2>Our Products page</h2>
+    <product_compo_1></product_compo_1>
+    <button v-on:click="IncreasePrice">Increase price</button>
+    <button v-on:click="DecreasePrice">Decrease price</button>
+    <product_compo_2></product_compo_2>
+  </div>
+</template>
+
+<script>
+import product_compo_1 from "@/components/product_compo_1.vue";
+import product_compo_2 from "@/components/product_compo_2.vue";
+export default {
+  name: "Products",
+  components: { product_compo_1, product_compo_2 },
+  props: ["productList1"],
+  data() {
+    return {};
+  },
+  methods: {
+    IncreasePrice: function () {
+      this.$store.state.productList.forEach((product) => {
+        product.price -= 1;
+      });
+    },
+    DecreasePrice() {
+      this.$store.state.productList.forEach((product) => {
+        product.price += 1;
+      });
+    },
+  },
+};
+</script>
